22a7a8f714123f51c5c7bc470224470fd6d6c25e
kpjonsson <philip.jonsson@gmail.com>
2017-10-17 21:19:28 -0400

    changed PS1

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -s $HOME/.dotfiles/.bash_profile $HOME
 ln -s $HOME/.dotfiles/.Rprofile $HOME
 ln -s $HOME/.dotfiles/.gitconfig $HOME
 ln -s $HOME/.dotfiles/.vimrc $HOME
+ln -s $HOME/.dotfiles/.inputrc $HOME
 
